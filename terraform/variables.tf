42d7ef75546d1105d5c486c64d9f41d986d26a90
maksim-se <a35b48@mail.ru>
2018-11-06 01:07:59 +0600

    main.tf -> db.tf и app.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,10 +11,6 @@ variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
 
-variable disk_image {
-  description = "Disk image"
-}
-
 variable private_key_path {
   description = "Path to the private key used for ssh access"
 }
@@ -27,4 +23,14 @@ variable zone {
 variable vm_count {
   description = "кол-во вм в кластере"
   default     = "1"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default = "reddit-db-base"
 }
